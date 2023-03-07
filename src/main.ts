8a32b84eed9a2774c1546bfe2ef542996b9cb51e
koni <test@q.com>
2023-03-07 23:13:43 +0800

    添加echarts

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { createApp } from 'vue'
 import './assets/css/reset.less'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import * as echarts from 'echarts'
 import App from './App.vue'
 
 const app = createApp(App)
+app.config.globalProperties.$echarts = echarts
 
 app.use(ElementPlus)
 app.mount('#app')
