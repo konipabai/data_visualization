d56703e82b1bd46b935728b1853a49aa27b25dbb
koni <test@q.com>
2023-04-02 19:22:37 +0800

    调整饼图样式细节，颜色

--- src/components/pie/index.vue
+++ src/components/pie/index.vue
@@ -1,6 +1,8 @@
 <template>
-    <div style="overflow:hidden;width: 100%;height: 100%;">
+    <div style="overflow:hidden;width: 100%;height: 100%;position: relative;">
+        <img src="../../assets/image/left_line.png" class="img-left">
         <div id="pie"></div>
+        <img src="../../assets/image/right_line.png" class="img-right">
     </div>
     
 </template>
@@ -14,7 +16,7 @@ const { proxy }: any = getCurrentInstance()
 const echarts = proxy.$echarts
 type EChartsOption = echarts.EChartsOption
 
-const colorList: string[] = ['#81EDD2', '#B0FA93'];
+const colorList: string[] = ['#9AE0EC', '#F18FBA'];
 const sum: number = sexData.reduce((per, cur) => per + cur.value, 0);
 const pieData1: pieType[] = [];
 const pieData2: pieType[] = [];
@@ -32,7 +34,7 @@ const manageData = () => {
             ...sexData[i],
             itemStyle: {
                 borderRadius: 10,
-                borderColor: "black",
+                borderColor: "#02095E",
                 borderWidth: 3
             },
         });
@@ -43,7 +45,7 @@ const manageData = () => {
                 color: colorList[i],
                 opacity: 0.21,
                 borderRadius: 5,
-                borderColor: "black",
+                borderColor: "#02095E",
                 borderWidth: 3
             },
         });
@@ -53,7 +55,7 @@ const manageData = () => {
             show: true,
             icon: "circle",
             left: lefts[i],
-            top: "45%",
+            top: "50%",
             itemStyle: {
                 color: colorList[i],
             },
@@ -62,7 +64,7 @@ const manageData = () => {
                 rich: {
                     legendTextStyle: {
                         fontSize: 15,
-                        color: "#ffffff",
+                        color: "#fff",
                     },
                     legendNumberStyle: {
                         fontSize: 15,
@@ -80,9 +82,10 @@ const initEcharts = () => {
     const option: EChartsOption = {
         title: {
             text: "2023年男女人口比例",
-            subtext: "总人数(单位:万)\n200000",
+            subtext: "总人数\n(单位:万)\n200000",
             left: "center",
-            itemGap: 150,
+            top: 20,
+            itemGap: 110,
             textStyle: {
                 color: "white",
                 fontSize: 18,
@@ -106,7 +109,7 @@ const initEcharts = () => {
         series: [
             {
                 type: 'pie',
-                radius: ['66%', '75%'],
+                radius: ['56%', '65%'],
                 center: ['50%', '57%'],
                 label: {
                     show: false
@@ -118,7 +121,7 @@ const initEcharts = () => {
             },
             {
                 type: 'pie',
-                radius: ['66%', '60%'],
+                radius: ['56%', '50%'],
                 center: ['50%', '57%'],
                 label: {
                     show: false
@@ -131,8 +134,8 @@ const initEcharts = () => {
             },
             {
                 type: 'gauge',
-                splitNumber: 70,
-                radius: '60%',
+                splitNumber: 60,
+                radius: '50%',
                 center: ['50%', '57%'],
                 startAngle: 0,
                 endAngle: 360,
@@ -157,7 +160,7 @@ const initEcharts = () => {
             {
                 type: 'pie',
                 center: ['50%', '57%'],
-                radius: [0, '45.6%'],
+                radius: [0, '50%'],
                 label: {
                     show: false
                 },
@@ -187,5 +190,15 @@ const initEcharts = () => {
 #pie {
     height: 100%;
     width: 100%;
+}
+.img-left {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+}
+.img-right {
+  position: absolute;
+  top: 0;
+  right: 0;
 }
 </style>
