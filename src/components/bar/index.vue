7a79c20f90713b08f8283a70e62e61390a13ca8d
koni <test@q.com>
2023-04-04 23:24:45 +0800

    修改柱状图样式

--- src/components/bar/index.vue
+++ src/components/bar/index.vue
@@ -1,6 +1,8 @@
 <template>
-    <div style="overflow:hidden;width: 100%;height: 100%;">
+    <div class="outer-box">
+        <img src="../../assets/image/left_line.png" class="img-left">
         <div id="bar"></div>
+        <img src="../../assets/image/right_line.png" class="img-right">
     </div>
 </template>
 
@@ -29,14 +31,21 @@ const initEcharts = () => {
     const option: EChartsOption = {
         title: {
             text: "2023年各国人口排行TOP5(单位:亿)",
+            left: "center",
+            top: '6%',
             textStyle: {
+                color: "white",
                 fontSize: 18,
             },
         },
         tooltip: {
-            trigger: "axis",
+            trigger: 'axis',
+            backgroundColor: "rgba(0, 0, 0, 0.7)",
+            textStyle: {
+                color: "#fff",
+            },
             axisPointer: {
-                type: "none",
+                type: 'none',
             },
             confine: true
         },
@@ -44,10 +53,10 @@ const initEcharts = () => {
             show: false,
         },
         grid: {
-            left: "18%",
-            right: "8%",
-            top: "10%",
-            bottom: "0%",
+            left: "16%",
+            right: "4%",
+            top: "19%",
+            bottom: "2%",
         },
         xAxis: {
             show: false,
@@ -65,28 +74,35 @@ const initEcharts = () => {
                 data: barDataCountry,
                 axisLabel: {
                     fontSize: 14,
-                    color: "black",
-                },
-            },
-            {
-                type: "category",
-                axisLine: {
-                    show: false,
-                },
-                axisTick: {
-                    show: false,
-                },
-                data: barDataValue,
-                axisLabel: {
-                    fontSize: 14,
-                    color: "black",
-                },
-            },
+                    color: "#ffffff",
+                }
+            }
         ],
         series: [
             {
+                name: '背景色',
+                type: 'bar',
+                barWidth: 16,
+                barGap: '-100%',
+                data: [15, 15, 15, 15, 15],
+                itemStyle: {
+                    color: 'rgba(50, 72, 135, 0.8)',
+                    borderRadius: [10, 10, 10, 10],
+                },
+                tooltip: {
+                    show: false
+                },
+            },
+            {
                 type: "bar",
-                barWidth: 21,
+                barWidth: 16,
+                label: {
+                    show: true,
+                    position: ['50%', '15%'],
+                    color: "#000000",
+                    fontSize: 14,
+                    fontWeight: "bold",
+                },
                 data: barData.map((item, i) => {
                     var itemStyle = {
                         color: colorList[i],
@@ -97,7 +113,7 @@ const initEcharts = () => {
                         itemStyle: itemStyle,
                     };
                 }),
-            },
+            }
         ],
     };
     myChart.setOption(option);
@@ -105,8 +121,28 @@ const initEcharts = () => {
 </script>
 
 <style scoped>
-#bar {
+.outer-box {
+    overflow: hidden;
     width: 100%;
     height: 100%;
+    position: relative;
+    background-color: rgba(204, 231, 239, 0.1);
+}
+
+#bar {
+    height: 100%;
+    width: 100%;
+}
+
+.img-left {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+}
+
+.img-right {
+    position: absolute;
+    top: 0;
+    right: 0;
 }
 </style>
