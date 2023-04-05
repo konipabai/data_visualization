e0983d874677f603f333a0936f15f465f694ff0a
koni <test@q.com>
2023-04-05 17:52:17 +0800

    githubActions的事前准备

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    chunkSizeWarningLimit: 4000
+  },
+  base: "/data_visualization/"
 })
