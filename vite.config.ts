1f03f3b40c816c0e0ccad76d39afb81897c73e5e
koni <test@q.com>
2023-04-05 18:54:45 +0800

    卸载gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  build: {
-    chunkSizeWarningLimit: 4000
-  },
-  base: "/data_visualization/"
+  plugins: [vue()]
 })
